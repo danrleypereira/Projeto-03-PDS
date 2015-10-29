619f062dcacea869a2de6bf317eb5b29e95fe0be
danrleywillyan <danrleywillian@gmail.com>
2015-10-28 22:22:22 -0200

    corrigido os problemas com as funções get. E implementação da função ler, ainda não totalmente implementada.

--- main.c
+++ main.c
@@ -7,7 +7,6 @@ Data de criação
 
 */
 
-
 #include "biblioteca_geral.h"
 #include "biblioteca_geral.c"
 #include "funcoes_CRUD.c"
@@ -16,16 +15,18 @@ int main()
 {
   int escolha;
 
-  printf("Digite 1: Adicionar um CRUD.");
-  printf("Digite 2: Acessar um CRUD.");
+  printf("Digite 1: Adicionar um CRUD.\n");
+  printf("Digite 2: Acessar um CRUD.\n");
+  printf("Digite 3: Atualizar um CRUD.\n");
+  printf("Digite 2: Deletar um CRUD.\n");
   scanf("%d", &escolha);
 
   switch (escolha) {
     case 1:
-      adicionar_crud(escolher_crud());
+      adicionar_crud();
       break;
     case 2:
-      ler(escolher_crud());
+      ler();
       break;
   }
 
